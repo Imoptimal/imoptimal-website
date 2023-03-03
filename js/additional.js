a0e37ffa87ca43b6811caffa09fc26ae7be0b916
Ivan Maljukanović <imoptimal@gmail.com>
2023-03-03 20:19:02 +0100

    Flip cards added

--- js/additional.js
+++ js/additional.js
@@ -2,41 +2,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     //Cookie consent
     cookieconsent.run({ "notice_banner_type": "headline", "consent_type": "simple", "palette": "light", "language": "en", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_privacy_policy_url": "https://imoptimal.com/privacy-policy/" });
-    // Add collapsible sections
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            var content = this.nextElementSibling;
-            if (content.style.visibility === "visible") {
-                content.style.visibility = "hidden";
-                content.style.zIndex = "-10";
-                this.textContent = "Show more info about this step";
-            } else {
-                content.style.visibility = "visible";
-                content.style.zIndex = "10";
-                this.textContent = "Hide the info";
-            }
-        });
-    }
-
-    // Close the show-more section
-    var close = document.getElementsByClassName("close");
-    for (i = 0; i < close.length; i++) {
-        close[i].addEventListener("click", function() {
-            var showMore = this.parentElement;
-            var buttonEl = showMore.previousElementSibling;
-            if (showMore.style.visibility === "visible") {
-                showMore.style.visibility = "hidden";
-                showMore.style.zIndex = "-10";
-                buttonEl.textContent = "Show more info about this step";
-            } else {
-                showMore.style.visibility = "visible";
-                showMore.style.zIndex = "10";
-                buttonEl.textContent = "Hide the info";
-            }
-        });
-    }
 
     // Add html template into another html file
     /*function includeHTML() {
