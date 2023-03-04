406598a3a9b75a6e7fef8f7149d7742991c6e6f7
Ivan Maljukanović <imoptimal@gmail.com>
2023-03-04 19:22:47 +0100

    Footer updated

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// When page has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Cookie consent
+    cookieconsent.run({ "notice_banner_type": "headline", "consent_type": "simple", "palette": "light", "language": "en", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_privacy_policy_url": "https://imoptimal.com/privacy-policy/" });
+
+    // Display CMS content
+    function displayCMS(rawGithubUrl, cmsEl, links, linksParent) {
+        links.forEach(function(link) {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                var clickedEl = e.target;
+                var file = rawGithubUrl + clickedEl.dataset.href;
+                if (clickedEl.classList.contains('show-cms')) {
+                    cmsEl.classList.add('displayed');
+                    fetch(file)
+                        .then(function(response) {
+                            // When the page is loaded convert it to text
+                            return response.text()
+                        })
+                        .then(function(html) {
+                            // Initialize the DOM parser
+                            var parser = new DOMParser();
+                            // Parse the text
+                            var page = parser.parseFromString(html, "text/html");
+                            var content = page.querySelector('.cms').innerHTML;
+                            cmsEl.innerHTML = content;
+                        });
+                }
+            });
+        });
+        // Hide CMS content
+        window.addEventListener('click', function() {
+            if (cmsEl.classList.contains('displayed')) {
+                cmsEl.classList.remove('displayed');
+            }
+        });
+        // Parent element
+        linksParent.addEventListener('click', function(e) {
+            e.stopPropagation();
+        });
+    }
+    var gitHubRepo = 'https://raw.githubusercontent.com/imoptimal/imoptimal-static-cms/main/';
+    var footerCms = document.querySelector('.footer #cms');
+    var footerLinks = document.querySelectorAll('.footer .links a');
+    var footerLinksParent = document.querySelector('.footer .links');
+    displayCMS(gitHubRepo, footerCms, footerLinks, footerLinksParent);
+});
