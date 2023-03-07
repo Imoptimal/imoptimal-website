0d8519649ac6490e4b843e2ce11eeafd488843f3
Ivan Maljukanović <imoptimal@gmail.com>
2023-03-07 18:10:02 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     cookieconsent.run({ "notice_banner_type": "headline", "consent_type": "simple", "palette": "light", "language": "en", "page_load_consent_levels": ["strictly-necessary"], "notice_banner_reject_button_hide": false, "preferences_center_close_button_hide": false, "page_refresh_confirmation_buttons": false, "website_privacy_policy_url": "https://imoptimal.com/privacy-policy/" });
 
     // Prevent img right click
-    var imgs = document.querySelector('img');
+    var imgs = document.querySelectorAll('img');
     imgs.forEach(function(img) {
         img.addEventListener('contextmenu', function(e) {
             e.preventDefault();
