be62d475c23a35ccbcb072c6277e8ac9232cba23
Ivan Maljukanović <imoptimal@gmail.com>
2023-03-07 18:44:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,13 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
     var lazyImgs = document.querySelectorAll('img.lazy-loading');
     lazyImgs.forEach(function(img) {
         var visible = isElementInViewport(img);
+        console.log(img);
         console.log(visible);
     });
 
