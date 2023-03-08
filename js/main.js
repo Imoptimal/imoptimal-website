703e3298eb4a6a1d8ba99f98eee80f51a146c712
imoptimal@gmail.com <imoptimal@gmail.com>
2023-03-08 16:26:04 +0100

    JS

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= ((window.innerHeight - 100) || (document.documentElement.clientHeight - 100)) &&
+            rect.bottom <= ((window.innerHeight + 100) || (document.documentElement.clientHeight + 100)) &&
             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
